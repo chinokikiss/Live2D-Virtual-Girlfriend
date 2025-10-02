12c678717cb00eadfd449676934239f7232a3d10
chinokikiss <158578443+chinokikiss@users.noreply.github.com>
2025-10-02 10:53:10 +0800

    Add files via upload

--- download.py
+++ download.py
@@ -1,15 +1,11 @@
 from modelscope import snapshot_download
 
-model_dir = 'models'
-
 campplus_path = snapshot_download(
     'iic/speech_campplus_sv_zh-cn_16k-common',
-    revision='v1.0.0',
-    cache_dir=model_dir
+    revision='v1.0.0'
 )
 
 sensevoice_path = snapshot_download(
     'iic/SenseVoiceSmall',
-    revision='master',
-    cache_dir=model_dir
+    revision='master'
 )
